0f948232e54d1abc2aa859ec9d0df0d187f28cdc
Phi Vo <phivo2@vanlanguni.vn>
2021-10-09 18:27:09 +0700

    commit project

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div>
+            <h4>Version 1.0.0</h4>
+            <h4>Coding by Phi</h4>
+            <h4>References: Traversy Media </h4>
+            <a href='/'>Go Back</a>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color ,text , onClick}) => {
+
+    return (
+    <button style={{backgroundColor:color}} onClick={onClick} className='btn'>{text}</button>
+    )
+}
+
+Button.defaultProps={
+    color:'steelblue'
+}
+
+Button.propTypes={
+    text:PropTypes.string,
+    color:PropTypes.string
+}
+
+export default Button
